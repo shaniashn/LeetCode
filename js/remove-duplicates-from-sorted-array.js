a72503890642f41fd4a47690ea4ae2b9f595532e
Sha Nia Siahaan <shaniashn76@gmail.com>
2024-10-29 23:14:55 +0700

    Update remove-duplicates-from-sorted-array.js

--- js/remove-duplicates-from-sorted-array.js
+++ js/remove-duplicates-from-sorted-array.js
@@ -7,8 +7,6 @@ var removeDuplicates = function(nums) {
   let k = 0;
 
   for(let i = 0; i < nums.length; i++){
-      
-      
       if (nums[i] !== nums[k]){
           k++;
           nums[k] = nums[i];
