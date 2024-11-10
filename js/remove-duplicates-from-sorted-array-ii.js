29250dee33e6bda2ee946ca3a0b6217da2fbba1d
Sha Nia Siahaan <shaniashn76@gmail.com>
2024-11-10 21:22:51 +0700

    Update remove-duplicates-from-sorted-array-ii.js

--- js/remove-duplicates-from-sorted-array-ii.js
+++ js/remove-duplicates-from-sorted-array-ii.js
@@ -1,42 +1,91 @@
 var removeDuplicates = function (nums) {
-  let r = 0;
-  let l = 0
-  
+  let j = 0;
+  let count = 1;
 
-  while(r < nums.length){
-    let count = 1;
-    while(nums[r] + 1 < nums.length && nums[r] == nums[r + 1]){
-      count++;
-      r++;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == nums[i-1]) {
+      count += 1; 
+    } else if (nums[i] !== nums[i-1]){
+      count = 1
     }
-
-    for(let j = 0; j < Math.min(2, count); j++)  {
-      nums[l] = nums[r]
-      l++
+    
+    if(count <= 2){
+      nums[j] = nums[i];
+      j++;
     }
-    r++
   }
-
-  return l;
+  return j;
 };
 
-console.log(removeDuplicates([1,1,1,2,2,3]));
+console.log(removeDuplicates([0,0,1,1,1,1,2,3,3]));
 
 
-// for(let r = 0; r < nums.length; r++) {
+// [0,0,1,1,1,1,2,3,3]
+// [1,1,1,2,2,3]
 
+// let j = 0;
+//   let count = 1;
 
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] == nums[i-1]) {
+//       count += 1; 
+//     } else if (nums[i] !== nums[i-1]){
+    //   count = 1
+    // }
 
+//     while (/* count > 2 && */ nums[i] !== nums[j]) {
+//       count = 1
+//       nums[j] = nums[i];
+      
+//     }
 
+//     if(count <= 2){
+//       j++;
+//     }
+//   }
+//   return nums;
 
+// ============
+// let j = 0;
+//   let count = 1;
 
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] == nums[i-1]) {
+//       count += 1;
+//     }
 
+//     while (/* count > 2 && */ nums[i] !== nums[j]) {
+//       nums[j] = nums[i];
+//       j++;
+//       count = 1
+//     }
 
+//     if (count <= 2) {
+//       j++;
+//     }
+//   }
+//   return nums;
 
+// =======
 
+// let r = 0;
+//   let l = 0
 
+//   while(r < nums.length){
+//     let count = 1;
+//     while(nums[r] + 1 < nums.length && nums[r] == nums[r + 1]){
+//       count++;
+//       r++;
+//     }
 
+//     for(let j = 0; j < Math.min(2, count); j++)  {
+//       nums[l] = nums[r]
+//       l++
+//     }
+//     r++
+//   }
 
+//   return l;
 
 
 // if(count < 2){   //k > 3
